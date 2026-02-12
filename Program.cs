b0795c766b959f916b7cbc41b7a0a6c8e5001995
ialikh72 <ialikh.w1@gmail.com>
2026-02-13 00:22:46 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class SUM
+{
+    static void Main()
+    {
+        int n;
+        Console.WriteLine("enter limit for sum:");
+        n=int.Parse(Console.ReadLine());
+        int sum = 0;
+        for(int i = 0; i <=n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("Sum={0}",sum);
+    }
+}
